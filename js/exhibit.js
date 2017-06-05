6c88476e604603fe7057725e5a9f1a8d0e78b388
vabarbosa <va@us.ibm.com>
2017-06-04 23:36:33 -0400

    show team facets in left rail

--- js/exhibit.js
+++ js/exhibit.js
@@ -8,7 +8,6 @@ $(document).ready(function() {
 
   var onBefore = function(q, options, paging, searchurl) {
     $('.simplesearch-more').prop('disabled', true)
-    $('.search-facets').slideUp(500)
   }
 
   var onFail = function(error) {
@@ -98,10 +97,6 @@ $(document).ready(function() {
     teamOnSuccess: teamOnSuccess
   }
 
-  $('.search-input').bind('click', function() {
-    $('.search-facets').slideDown(750)
-  })
-
   if (typeof repos !== 'undefined' && repos !== null && repos.length > 0) {
     var total = repos.length
 
@@ -153,4 +148,3 @@ $(document).ready(function() {
     });
   }
 })
-          
