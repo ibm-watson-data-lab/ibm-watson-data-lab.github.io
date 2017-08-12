6a4949bea3e40e8594c50b4ed7e435ea3932b57a
vabarbosa <va@us.ibm.com>
2017-08-12 02:52:54 -0400

    update link to profile page

--- js/exhibit.js
+++ js/exhibit.js
@@ -1,5 +1,9 @@
 /* global $, location */
 $(document).ready(function () {
+  var searchuserGithub = 'https://wdp-advo-team.mybluemix.net/search?q=%22github%20username%22:{username}&limit=1'
+  var searchuserTwitter = 'https://wdp-advo-team.mybluemix.net/search?q=%22twitter%20username%22:{username}&limit=1'
+  var searchuserMedium = 'https://wdp-advo-team.mybluemix.net/search?q=%22medium%20username%22:{username}&limit=1'
+
   $('.button-collapse').sideNav()
   $('.modal').modal()
 
@@ -65,16 +69,19 @@ $(document).ready(function () {
     var connecticons = []
     if (fields['github username']) {
       fields['profile icon'] = '<img src="http://github.com/' + fields['github username'] + '.png?size=200" />'
+      fields.linkhash = 'g/' + fields['github username']
       connect.push(' <a href="http://www.github.com/' + fields['github username'] + '" target="_blank">Github</a>')
       connecticons.push('<a href="http://www.github.com/' + fields['github username'] + '" title="GitHub" target="_blank"><i class="fa fa-github" aria-hidden="true"></i></a>')
     }
-    if (fields['twitter username']) {
-      connect.push(' <a href="http://www.twitter.com/' + fields['twitter username'] + '" target="_blank">' + (fields['twitter username'].indexOf('@') === -1 ? ('@' + fields['twitter username']) : fields['twitter username']) + '</a>')
-      connecticons.push('<a href="https://twitter.com/' + fields['twitter username'] + '" title="Twitter" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>')
-    }
     if (fields['medium username']) {
+      fields.linkhash = fields.linkhash || ('m/' + fields['medium username'])
       connect.push(' <a href="https://medium.com/' + (fields['medium username'].indexOf('@') === -1 ? ('@' + fields['medium username']) : fields['medium username']) + '" target="_blank">Medium</a>')
       connecticons.push('<a href="https://medium.com/' + (fields['medium username'].indexOf('@') === -1 ? ('@' + fields['medium username']) : fields['medium username']) + '" title="Medium" target="_blank"><i class="fa fa-medium" aria-hidden="true"></i></a>')
+    }
+    if (fields['twitter username']) {
+      fields.linkhash = fields.linkhash || ('t/' + fields['twitter username'])
+      connect.push(' <a href="http://www.twitter.com/' + fields['twitter username'] + '" target="_blank">' + (fields['twitter username'].indexOf('@') === -1 ? ('@' + fields['twitter username']) : fields['twitter username']) + '</a>')
+      connecticons.push('<a href="https://twitter.com/' + fields['twitter username'] + '" title="Twitter" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>')
     }
     if (fields['linkedin url']) {
       connect.push(' <a href="' + fields['linkedin url'] + '" target="_blank">LinkedIn</a>')
@@ -90,35 +97,53 @@ $(document).ready(function () {
     }
 
     fields.connect = connect
-    fields.connecticons = connecticons.join(' &nbsp; ')
+    fields.connecticons = connecticons.join(' ')
     // fields.tldr = [fields.specialties, fields.languages, fields.connect].join(', ')
 
     return fields
   }
 
   var teamHashChanged = function () {
-    if (location.hash && location.pathname.startsWith('/team')) {
-      var hash = location.hash.charAt(1) === '/' ? location.hash.substring(2) : location.hash.substring(1)
-      $('#team_search').hide()
-      $('#team_profile').html('loading...').show()
-      var url = 'https://wdp-advo-team.mybluemix.net/search?q=%22github%20username%22:' + hash + '&limit=1'
-      $.getJSON(url).then(function (data) {
-        var advocate = data && data.rows.length ? data.rows[0] : null
-        if (advocate) {
-          var userdata = prepUserData(advocate)
+    var hash = null
+    if (location.hash && location.pathname.indexOf('/team') === 0) {
+      hash = location.hash.charAt(1) === '/' ? location.hash.substring(2) : location.hash.substring(1)
+      if (hash) {
+        $('#team_search').hide()
+        $('#team_profile').html('loading...').show()
 
-          var tmpl = $('#team_member_profile')
-            .html()
-            .replace(/\{\{(.+?)\}\}/g, function ($0, $1) {
-              return userdata.hasOwnProperty($1) ? userdata[$1] : '' // $0
-            })
-
-          $('#team_profile').html(tmpl).show()
+        var url = null
+        if (hash.indexOf('g/') === 0) {
+          url = searchuserGithub.replace('{username}', hash.substring(2))
+        } else if (hash.indexOf('m/') === 0) {
+          url = searchuserMedium.replace('{username}', hash.substring(2))
+        } else if (hash.indexOf('t/') === 0) {
+          url = searchuserTwitter.replace('{username}', hash.substring(2))
         } else {
-          $('#team_profile').html('<p>nothing to see here</p>').show()
+          hash = null
         }
-      })
-    } else {
+
+        if (url) {
+          $.getJSON(url).then(function (data) {
+            var advocate = data && data.rows.length ? data.rows[0] : null
+            if (advocate) {
+              var userdata = prepUserData(advocate)
+
+              var tmpl = $('#team_member_profile')
+                .html()
+                .replace(/\{\{(.+?)\}\}/g, function ($0, $1) {
+                  return userdata.hasOwnProperty($1) ? userdata[$1] : '' // $0
+                })
+
+              $('#team_profile').html(tmpl).show()
+            } else {
+              $('#team_profile').html('<p>nothing to see here</p>').show()
+            }
+          })
+        }
+      }
+    }
+
+    if (!hash) {
       $('#team_profile').hide()
       $('#team_search').show()
     }
